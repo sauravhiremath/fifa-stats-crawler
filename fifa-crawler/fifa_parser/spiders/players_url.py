8b1cdafc714b7170aabd894f2b82923644faf041
Saurav M. H <sauravhiremath@gmail.com>
2020-04-22 09:15:25 +0530

    fix: Added faster implementation for urls

--- fifa-crawler/fifa_parser/spiders/players_url.py
+++ fifa-crawler/fifa_parser/spiders/players_url.py
@@ -8,7 +8,7 @@ class SofifaSpider(scrapy.Spider):
 
 	def start_requests(self):
 		urls = [
-		'https://sofifa.com/players?offset=0'
+		'https://sofifa.com/players'
 		]
 
 		for url in urls:
@@ -16,14 +16,14 @@ class SofifaSpider(scrapy.Spider):
 
 	def parse(self, response):
 		print(self.pages)
-		for player in response.css('.col-name'):
-			player_link = player.xpath('a/@href').re(r'/player/\w+')
+		for player in response.css('tbody>tr'):
+			player_link = player.xpath('.//a/@href').re(r'/player/\w+')
 			if len(player_link)	> 0:
 				yield {
 					'player_url': player_link[0]
 				}
 
-		next_page = response.xpath('.//a[@class="bp3-button pjax"]/@href').extract()
+		next_page = response.xpath('.//a[@class="bp3-button pjax"]/@href').getall()
 		if next_page:
 			if len(next_page) == 1:
 				next_href = next_page[0]
