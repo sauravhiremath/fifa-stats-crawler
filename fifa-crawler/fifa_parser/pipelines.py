f1a908a371738347371d600ff8235f02f927e073
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-28 13:06:24 +0530

    feat: add spider check in pipeline

--- fifa-crawler/fifa_parser/pipelines.py
+++ fifa-crawler/fifa_parser/pipelines.py
@@ -7,16 +7,21 @@ from scrapy.exporters import JsonItemExporter
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+
 class JsonPipeline(object):
-    def __init__(self):
-        self.file = open("../data/json/players_stats.json", 'wb')
-        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+    def open_spider(self, spider):
+        if spider.name == 'players_stats':
+            self.file = open("../data/json/players_stats.json", 'wb')
+        elif spider.name == 'players_url':
+            self.file = open("../data/json/players_url.json", 'wb')
+        self.exporter = JsonItemExporter(
+            self.file, encoding='utf-8', ensure_ascii=False)
         self.exporter.start_exporting()
- 
+
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
- 
+
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
